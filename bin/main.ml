e1c500f92bef3cdf7198f0c8dfa3963d10866f54
jhubaum <johannes.huwald@gmail.com>
2023-12-26 21:35:54 +0100

    Add print module function and command line parser

--- bin/main.ml
+++ bin/main.ml
@@ -1,16 +1,38 @@
-(*
 open Parser;;
 
-let string_of_module _mod =
-    let indent_with len = String.make len ' ' in
-    let string_of_function_call indent (call : function_call) = 
-        (indent_with indent) ^ call.name ^ "(" ^ (String.concat ", " call.args) ^ ")" in
-    let string_of_scope scope indent = 
-        "{" ^ (String.concat "\n" (List.map (string_of_function_call (indent+2)) scope)) ^ "}" in
-    let string_of_func_def func = func.name ^ (string_of_scope func.scope 2) in
-    List.fold_left (fun s func -> s ^ (string_of_func_def func) ^ "\n") "" _mod
-    *)
+let print_with_indent indent strings = 
+  print_string (String.make indent ' ');
+  List.iter print_string strings;
+  print_newline ()
 
-let () = 
-    let _mod = Parser.parse_file "test.trout" in
-    Interpreter.run _mod;
+
+let print_module _mod = 
+  let value_to_string = String.escaped in
+  let args_list_to_string args = "(" ^ (String.concat ", " (List.map value_to_string args)) ^ ")" in
+  let print_scope indent scope = 
+    let print_statement (statement : statement) = 
+      print_with_indent indent [statement.name; args_list_to_string statement.args; ";"] in
+    List.iter print_statement scope in
+  let print_function indent func = 
+    print_with_indent indent ["fn "; func.name; "() {"];
+    print_scope (indent+4) func.scope;
+    print_with_indent indent ["}\n"] in
+  List.iter (print_function 0) _mod
+
+type options = { filename: string; unwrap: bool; }
+
+let parse_args =
+  let input_file = ref ""  in 
+  let flag = ref false in
+  let usage_msg = "Usage: trout [options] filename" in
+  let speclist =
+    [
+      ("--unwrap", Arg.Set flag, "Print the unwrapped code for the given file")
+    ] in
+  Arg.parse speclist (fun s -> input_file := s) usage_msg;
+  { filename = !input_file; unwrap = !flag }
+
+let () =
+  let args = parse_args in
+  let _mod = Parser.parse_file args.filename in
+  if args.unwrap then print_module _mod else Interpreter.run _mod;
